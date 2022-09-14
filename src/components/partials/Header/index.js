accb5c730c75d66301f7d7d005f0921a04c0a312
dennyazevedofh <denny.filho@outlook.com>
2022-09-13 22:09:57 -0300

    criado NotFound e Login

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { HeaderArea } from './styled'
 import { Link } from 'react-router-dom'
+import { isLogged } from '../../../helpers/AuthHandler'
 
 const Header = () => {
+	let logged = isLogged()
+
 	return (
 		<HeaderArea>
 			<div className="container">
@@ -15,15 +18,32 @@ const Header = () => {
 				</div>
 				<nav>
 					<ul>
-						<li>
-							<Link to="/signin">Login</Link>
-						</li>
-						<li>
-							<Link to="/signup">Cadatrar</Link>
-						</li>
-						<li>
-							<Link to="/signin" className="button">Postar um anúncio</Link>
-						</li>
+						{!logged &&
+							<>
+								<li>
+									<Link to="/signin">Login</Link>
+								</li>
+								<li>
+									<Link to="/signup">Cadatrar</Link>
+								</li>
+								<li>
+									<Link to="/signin" className="button">Postar um anúncio</Link>
+								</li>
+							</>
+						}
+						{logged &&
+							<>
+								<li>
+									<Link to="/my-account">Minha Conta</Link>
+								</li>
+								<li>
+									<Link to="/logout">Sair</Link>
+								</li>
+								<li>
+									<Link to="/post-an-ad" className="button">Postar um anúncio</Link>
+								</li>
+							</>
+						}
 					</ul>
 				</nav>
 			</div>
